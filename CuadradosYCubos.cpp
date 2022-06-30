5d8c200e3c6cae8d6e461b364af5233c8415b4a0
FaridCG343 <71570619+FaridCG343@users.noreply.github.com>
2022-06-30 08:37:47 -0600

    Add files via upload

--- CuadradosYCubos.cpp
+++ CuadradosYCubos.cpp
@@ -1,48 +1,17 @@
 #include<iostream>
 #include<cmath>
 using namespace std;
-int numCC(int num){
-    int con=2,cont=0;
-    int pot;
-    while (true)
-    {
-        pot=pow(con,2);
-        if (pot>num)
-        {
-            break;
-        }
-        if(pot<=num){
-            cont++;
-            con++;
-        }
-    }
-    con=2;
-    while (true)
-    {
-        pot=pow(con,3);
-        if (pot>num)
-        {
-            break;
-        }
-        if(pot<=num){
-            cont++;
-            con++;
-        }
-    }
-    return cont+1;
-}
+
 int main(int argc, char const *argv[])
 {
-    int tc,num;
+    int tc,num,res1,res2;
 
     cin>>tc;
     while(tc--){
         cin>>num;
-        if(num==1){
-            cout<<"1"<<endl;
-        }else{
-            cout<<numCC(num)<<endl;
-        }
+        res1=sqrt(num);
+        res2=pow(num,(1.0/3.0));
+        cout<<res1+res2-1<<endl;
     }    
     return 0;
 }
